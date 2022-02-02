8d85062337eb15f68adaefef976c6721de38047e
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-02-02 14:15:13 -0500

    Add EventSerializer

---
+++ erlportal/events/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+
+from .models import Event
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = ['uid', 'title', 'eventType', 'year', 'month', 'day', 'startTime', 'endTime', 'description', 'slug']
