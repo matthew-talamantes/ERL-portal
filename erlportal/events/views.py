4777fda6dee20d7d12c9dde668fc4596b1895459
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-02-10 09:37:24 -0500

    Change Event_List to class view

--- erlportal/events/views.py
+++ erlportal/events/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, Http404
 from django.views.decorators.csrf import csrf_exempt
 
 # from rest_framework.parsers import JSONParser
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Event
 from .serializers import EventSerializer
@@ -13,22 +14,39 @@ from .serializers import EventSerializer
 
 # Create your views here.
 
-@api_view(['GET', 'POST'])
-def event_list(request, format=None):
+
+class EventList(APIView):
     """
-        List all events or create new event
+        List all events, or create a new event
     """
-    if request.method == 'GET':
+    def get(self, request, format=None):
         events = Event.objects.all()
         serializer = EventSerializer(events, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         serializer = EventSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# @api_view(['GET', 'POST'])
+# def event_list(request, format=None):
+#     """
+#         List all events or create new event
+#     """
+#     if request.method == 'GET':
+#         events = Event.objects.all()
+#         serializer = EventSerializer(events, many=True)
+#         return Response(serializer.data)
+
+#     elif request.method == 'POST':
+#         serializer = EventSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def event_detail(request, slug, format=None):
