abd449719fc875a7d4bf2b2f5032c1310f140c42
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-02-10 12:31:24 -0500

    Set lookup_field to slug for EventDetail

--- erlportal/events/views.py
+++ erlportal/events/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse, Http404
-from django.views.decorators.csrf import csrf_exempt
+# from django.shortcuts import render
+# from django.http import HttpResponse, JsonResponse, Http404
+# from django.views.decorators.csrf import csrf_exempt
 
 # from rest_framework.parsers import JSONParser
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.views import APIView
+# from rest_framework import status
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+# from rest_framework.views import APIView
+
+from rest_framework import generics
 
 from .models import Event
 from .serializers import EventSerializer
@@ -15,21 +17,12 @@ from .serializers import EventSerializer
 # Create your views here.
 
 
-class EventList(APIView):
+class EventList(generics.ListCreateAPIView):
     """
         List all events, or create a new event
     """
-    def get(self, request, format=None):
-        events = Event.objects.all()
-        serializer = EventSerializer(events, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = EventSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
 
 # @api_view(['GET', 'POST'])
 # def event_list(request, format=None):
@@ -48,33 +41,13 @@ class EventList(APIView):
 #             return Response(serializer.data, status=status.HTTP_201_CREATED)
 #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class EventDetail(APIView):
+class EventDetail(generics.RetrieveUpdateDestroyAPIView):
     """
         Retrieve, update or delete a snippet instance.
     """
-    def get_object(self, slug):
-        try:
-            return Event.objects.get(slug=slug)
-        except Event.DoesNotExist:
-            raise Http404
-
-    def get(self, request, slug, format=None):
-        event = self.get_object(slug)
-        serializer = EventSerializer(event)
-        return Response(serializer.data)
-
-    def put(self, request, slug, format=None):
-        event = self.get_object(slug)
-        serializer = EventSerializer(event, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, slug, format=None):
-        event = self.get_object(slug)
-        event.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    lookup_field = 'slug'
 
 # @api_view(['GET', 'PUT', 'DELETE'])
 # def event_detail(request, slug, format=None):
