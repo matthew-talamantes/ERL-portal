1786f589d4c1831e21697fb9aee454c1dd30687f
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-02-16 12:09:21 -0500

    Add url paths for pages app

--- erlportal/erlportal/urls.py
+++ erlportal/erlportal/urls.py
@@ -21,8 +21,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('', include('pages.urls')),
     # For rest tutorial rewrite for actual release
-    path('', include('events.urls')),
+    path('api/', include('events.urls')),
 ]
 
 if settings.DEBUG:
