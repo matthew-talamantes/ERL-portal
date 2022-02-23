a4d8eaaeef550baf3223a70f493a8ab1d2a6800d
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-02-22 21:39:11 -0500

    Make profile form and signup form inline

--- erlportal/useraccount/forms.py
+++ erlportal/useraccount/forms.py
@@ -33,34 +33,36 @@ class ProfileUpdateForm(forms.ModelForm):
         fields = ['image', 'firstName', 'middleName', 'lastName', 'phoneNumber',
             'shareName', 'contactPreference', 'birthDate']
 
-# class ErlSignupForm(SignupForm):
+class ErlSignupForm(SignupForm):
 
-#     PHONE = 'phone'
-#     EMAIL = 'email'
-#     contactChoices = [(None, ''), (EMAIL, 'E-Mail'), (PHONE, 'Phone')]
+    PHONE = 'phone'
+    EMAIL = 'email'
+    contactChoices = [(None, ''), (EMAIL, 'E-Mail'), (PHONE, 'Phone')]
 
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         self.fields['firstName'] = forms.CharField(label="First Name", max_length=25, widget=forms.TextInput(attrs={'placeholder': 'First Name'}))
-#         self.fields['middleName'] = forms.CharField(label="Middle Name", max_length=25, widget=forms.TextInput(attrs={'placeholder': 'Middle Name'}), required=False)
-#         self.fields['lastName'] = forms.CharField(label="Last Name", max_length=25, widget=forms.TextInput(attrs={'placeholder': 'Last Name'}))
-#         self.fields['shareName'] = forms.BooleanField(label="Share Name", required=False)
-#         self.fields['phoneNumber'] = PhoneNumberField(label='Phone Number', widget=forms.TextInput(attrs={'placeholder': '+17031234567'}))
-#         self.fields['contactPreference'] = forms.ChoiceField(label='Contact Preference', choices=self.contactChoices)
-#         self.fields['birthDate'] = forms.DateField(label='Birthdate', input_formats=['%m-%d-%Y'], widget=forms.DateInput(attrs={'placeholder': 'MM-DD-YYYY'}))
-#         self.fields['image'] = forms.ImageField(label='Profile Image', required=False)
+    def __init__(self, *args, **kwargs):
+
+        self.imgFile = kwargs.pop('imgFile')
+        super().__init__(*args, **kwargs)
+        self.fields['firstName'] = forms.CharField(label="First Name", max_length=25, widget=forms.TextInput(attrs={'placeholder': 'First Name'}))
+        self.fields['middleName'] = forms.CharField(label="Middle Name", max_length=25, widget=forms.TextInput(attrs={'placeholder': 'Middle Name'}), required=False)
+        self.fields['lastName'] = forms.CharField(label="Last Name", max_length=25, widget=forms.TextInput(attrs={'placeholder': 'Last Name'}))
+        self.fields['shareName'] = forms.BooleanField(label="Share Name", required=False)
+        self.fields['phoneNumber'] = PhoneNumberField(label='Phone Number', widget=forms.TextInput(attrs={'placeholder': '+17031234567'}))
+        self.fields['contactPreference'] = forms.ChoiceField(label='Contact Preference', choices=self.contactChoices)
+        self.fields['birthDate'] = forms.DateField(label='Birthdate', input_formats=['%m-%d-%Y'], widget=forms.DateInput(attrs={'placeholder': 'MM-DD-YYYY'}))
+        self.fields['image'] = forms.ImageField(label='Profile Image', required=False)
     
 
-#     def clean_image(self):
-#         print('hello')
-#         print('world')
-#         # file_data = {'image': SimpleUploadedFile(self.cleaned_data['image'])}
+    def clean_image(self):
+        print('hello')
+        print('world')
+        # file_data = {'image': SimpleUploadedFile(self.cleaned_data['image'], self.imgFile['image'])}
 
-#     def save(self, request):
-#         user = super().save(request)
-#         data =self.cleaned_data
-#         # custom processing here
-#         profile = Profile(user=user, image=data['image'], firstName=data['firstName'], middleName=data['middleName'], lastName=data['lastName'], phoneNumber=data['phoneNumber'], shareName=data['shareName'], contactPreference=data['contactPreference'], birthDate=data['birthDate'])
-#         profile.save()
-#         return user
-# ProfileFormSet = inlineformset_factory(ErlUser, Profile, form=ProfileCreateForm, extra=2)
+    def save(self, request):
+        user = super().save(request)
+        data =self.cleaned_data
+        # custom processing here
+        profile = Profile(user=user, image=self.imgFile['image'], firstName=data['firstName'], middleName=data['middleName'], lastName=data['lastName'], phoneNumber=data['phoneNumber'], shareName=data['shareName'], contactPreference=data['contactPreference'], birthDate=data['birthDate'])
+        profile.save()
+        return user
+
--- erlportal/useraccount/views.py
+++ erlportal/useraccount/views.py
@@ -5,7 +5,7 @@ from django.views.generic import DetailView
 
 from allauth.account.views import SignupView
 
-from .forms import ProfileUpdateForm
+from .forms import ProfileUpdateForm, ErlSignupForm
 from .models import Profile
 
 # Create your views here.
@@ -25,28 +25,14 @@ def profile(request):
     }
     return render(request, 'useraccounts/profile.html', context)
 
-# class ErlSignup(SignupView):
-#     template_name = 'account/signup.html'
-#     profileForm = ProfileUpdateForm()
+class ErlSignup(SignupView):
+    template_name = 'account/signup.html'
+    form_class = ErlSignupForm
 
-#     def form_valid(self, form):
-#         context = self.get_context_data()
-#         signupForm = context['form']
-#         profileForm = context['profileForm']
-#         if signupForm.is_valid():
-#             user = signupForm.save(self.request)
-#             profileForm.user = user
-#         if signupForm.is_valid() and profileForm.is_valid():
-#             signupForm.save()
-#             profileForm.save()
-#             super().form_valid(form)
-
-
-
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['profileForm'] = self.profileForm
-#         return context
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['imgFile'] = self.request.FILES
+        return kwargs
 
 class UserProfile(DetailView):
     model = Profile
