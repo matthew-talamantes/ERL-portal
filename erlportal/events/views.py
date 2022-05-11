3bd1449678b4d6973ea52b59a21c5c958c39f3a7
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-05-11 16:41:09 -0400

    Remove commented out code

--- erlportal/events/views.py
+++ erlportal/events/views.py
@@ -24,23 +24,6 @@ class EventList(generics.ListCreateAPIView):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
 
-# @api_view(['GET', 'POST'])
-# def event_list(request, format=None):
-#     """
-#         List all events or create new event
-#     """
-#     if request.method == 'GET':
-#         events = Event.objects.all()
-#         serializer = EventSerializer(events, many=True)
-#         return Response(serializer.data)
-
-#     elif request.method == 'POST':
-#         serializer = EventSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 class EventDetail(generics.RetrieveUpdateDestroyAPIView):
     """
         Retrieve, update or delete a event instance.
@@ -49,25 +32,4 @@ class EventDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = EventSerializer
     lookup_field = 'slug'
 
-# @api_view(['GET', 'PUT', 'DELETE'])
-# def event_detail(request, slug, format=None):
-#     """
-#         Retrieve, update or delete an event.
-#     """
-#     try:
-#         event = Event.objects.get(slug=slug)
-#     except Event.DoesNotExist:
-#         return Response(status=status.HTTP_404_NOT_FOUND)
 
-#     if request.method == 'GET':
-#         serializer = EventSerializer(event)
-#         return Response(serializer.data)
-#     elif request.method == 'PUT':
-#         serializer = EventSerializer(event, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#     elif request.method == 'DELETE':
-#         event.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
