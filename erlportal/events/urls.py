21cafc65d10c57d284ce1d277818697f63852f9a
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-05-11 19:30:16 -0400

    Update paths and add CaldendarView path

--- erlportal/events/urls.py
+++ erlportal/events/urls.py
@@ -5,9 +5,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('', include('rest_framework.urls')),
-    path('events/', views.EventList.as_view(), name='event-list-endpoint'),
-    path('events/<slug:slug>/', views.EventDetail.as_view(), name='event-detail-endpoint'),
+    path('api/', include('rest_framework.urls')),
+    path('api/events', views.EventList.as_view(), name='event-list-endpoint'),
+    path('api/event/<slug:slug>/', views.EventDetail.as_view(), name='event-detail-endpoint'),
+    path('calendar/<int:year>/<int:month>/', views.CalendarView.as_view(), name='events-calendar'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
