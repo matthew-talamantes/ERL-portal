132f7afe7bdfd030d722313738a0cc0f5c92ceb9
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-05-13 18:49:09 -0400

    Add event urls

--- erlportal/events/urls.py
+++ erlportal/events/urls.py
@@ -5,6 +5,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
+    path('event/new/', views.EventCreateView.as_view(), name='event-create'),
+    path('event/<slug:slug>/', views.EventDetailView.as_view(), name='event-detail'),
+    path('event/<slug:slug>/update/', views.EventUpdateView.as_view(), name='event-update'),
+    path('event/<slug:slug>/delete/', views.EventDeleteView.as_view(), name='event-delete'),
     path('api/', include('rest_framework.urls')),
     path('api/events', views.EventList.as_view(), name='event-list-endpoint'),
     path('api/event/<slug:slug>/', views.EventDetail.as_view(), name='event-detail-endpoint'),
