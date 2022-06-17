11e171ceb43760e28ce922cabc923e9b1fbe2266
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-06-17 17:12:10 -0400

    Finish BaseShift model

--- erlportal/shifts/models.py
+++ erlportal/shifts/models.py
@@ -3,6 +3,8 @@ from django.template.defaultfilters import slugify
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 from django.utils import timezone
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
 import uuid
 
@@ -36,7 +38,28 @@ class BaseShift(models.Model):
     endRepeat = models.DateField(verbose_name='End Repeat', blank=True)
     staffSlots = models.IntegerField(blank=False)
     volSlots = models.IntegerField(blank=False)
+    minSlots = models.IntegerField(verbose_name='Minimum Staff/volunteers', blank=False)
     defaultStaff = models.ManyToManyField(ErlUser, related_name='default_staff', blank=True, null=True, on_delete=models.SET_NULL)
     defaultVols = models.ManyToManyField(ErlUser, related_name='default_vols', blank=True, null=True, on_delete=models.SET_NULL)
+    slug = models.SlugField(blank=True, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    def clean(self):
+        if self.endTime <= self.startTime:
+            raise ValidationError(_('Must set an end time that is after the start time.'))
+
+        if (int(self.staffSlots) + int(self.volSlots)) < int(self.minSlots):
+            slotDiff = self.minSlots - (self.staffSlots + self.volSlots)
+            self.volSlots += slotDiff
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('base-shift-detail', kwargs={'slug': self.slug})
 
 
