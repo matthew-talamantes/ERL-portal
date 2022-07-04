b842112c7f6d50215fcfef7cb54ff9266e7935a5
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-07-04 15:19:12 -0400

    Update baseshift views

--- erlportal/shifts/views.py
+++ erlportal/shifts/views.py
@@ -13,23 +13,38 @@ from django.forms import TextInput
 from .models import BaseShift
 
 # Create your views here.
-class BaseShiftListView(LoginRequiredMixin, ListView):
+class BaseShiftListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
     model = BaseShift
     context_object_name = 'shifts'
     ordering = ['name']
     paginate_by = 25
 
-class BaseShiftDetailView(LoginRequiredMixin, DetailView):
+    def test_func(self):
+        user = self.request.user
+        if user.groups.filter(name='Staff').exists() or user.groups.filter(name='WebAdmin').exists():
+            return True
+        else:
+            return False
+
+class BaseShiftDetailView(LoginRequiredMixin, UserPassesTestMixin, DetailView):
     model = BaseShift
 
-class BaseShiftCreateView(LoginRequiredMixin, CreateView):
+    def test_func(self):
+        user = self.request.user
+        if user.groups.filter(name='Staff').exists() or user.groups.filter(name='WebAdmin').exists():
+            return True
+        else:
+            return False
+
+class BaseShiftCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = BaseShift
-    fields = ['name', 'description', 'startTime', 'endTime', 'repeat', 'endRepeat', 'minSlots', 'staffSlots', 'volSlots', 'defaultStaff', 'defaultVols']
+    fields = ['name', 'description', 'date', 'startTime', 'endTime', 'repeat', 'endRepeat', 'minSlots', 'staffSlots', 'volSlots', 'defaultStaff', 'defaultVols']
 
     def get_form(self):
         form = super().get_form()
-        form.fields['startTime'].widget = TextInput(attrs={'type': 'datetime-local'})
-        form.fields['endTime'].widget = TextInput(attrs={'type': 'datetime-local'})
+        form.fields['date'].widget = TextInput(attrs={'type': 'date'})
+        form.fields['startTime'].widget = TextInput(attrs={'type': 'time'})
+        form.fields['endTime'].widget = TextInput(attrs={'type': 'time'})
         form.fields['endRepeat'].widget = TextInput(attrs={'type': 'date'})
         return form
 
@@ -37,17 +52,39 @@ class BaseShiftCreateView(LoginRequiredMixin, CreateView):
         form.instance.createdBy = self.request.user
         return super().form_valid(form)
 
-class BaseShiftUpdateView(LoginRequiredMixin, UpdateView):
+    def test_func(self):
+        user = self.request.user
+        if user.groups.filter(name='Staff').exists() or user.groups.filter(name='WebAdmin').exists():
+            return True
+        else:
+            return False
+
+class BaseShiftUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = BaseShift
-    fields = ['name', 'description', 'startTime', 'endTime', 'repeat', 'endRepeat', 'minSlots', 'staffSlots', 'volSlots', 'defaultStaff', 'defaultVols']
+    fields = ['name', 'description', 'date', 'startTime', 'endTime', 'repeat', 'endRepeat', 'minSlots', 'staffSlots', 'volSlots', 'defaultStaff', 'defaultVols']
 
     def get_form(self):
         form = super().get_form()
-        form.fields['startTime'].widget = TextInput(attrs={'type': 'datetime-local'})
-        form.fields['endTime'].widget = TextInput(attrs={'type': 'datetime-local'})
+        form.fields['date'].widget = TextInput(attrs={'type': 'date'})
+        form.fields['startTime'].widget = TextInput(attrs={'type': 'time'})
+        form.fields['endTime'].widget = TextInput(attrs={'type': 'time'})
         form.fields['endRepeat'].widget = TextInput(attrs={'type': 'date'})
         return form
 
-class BaseShiftDeleteView(LoginRequiredMixin, DeleteView):
+    def test_func(self):
+        user = self.request.user
+        if user.groups.filter(name='Staff').exists() or user.groups.filter(name='WebAdmin').exists():
+            return True
+        else:
+            return False
+
+class BaseShiftDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = BaseShift
     success_url = '/'
+
+    def test_func(self):
+        user = self.request.user
+        if user.groups.filter(name='Staff').exists() or user.groups.filter(name='WebAdmin').exists():
+            return True
+        else:
+            return False
