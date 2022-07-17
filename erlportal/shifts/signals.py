3f0b8e4f06d2a3a15111ef5adec32c395c12846c
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-07-17 17:31:48 -0400

    Add signals to add staff and vols to shiftInstance

--- erlportal/shifts/signals.py
+++ erlportal/shifts/signals.py
@@ -1,8 +1,10 @@
 from django.dispatch import receiver
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, m2m_changed
 from django.db import transaction
 
 from datetime import date, timedelta
+
+from useraccount.models import ErlUser
 
 from .models import BaseShift, ShiftInstance
 
@@ -17,13 +19,11 @@ def create_instance_shifts(sender, instance, created, **kwargs):
                     endRepeat = instance.endRepeat
                 else:
                     endRepeat = date(instance.date.year + 100, instance.date.month, instance.date.day)
-                    
+
                 shiftList = []
                 shiftDate = instance.date
-                while shiftDate < endRepeat:
-                    shift = ShiftInstance(baseShift=instance, date=shiftDate, startTime=instance.starTime, endTime=instance.endTime, name=instance.name, description=instance.description, staffSlots=instance.staffSlots, volSlots=instance.volSlots, minSlots=instance.minSlots, slots=instance.slots)
-                    shift.staff.add(instance.defaultStaff.all())
-                    shift.vols.add(instance.defaultVols.all())
+                while shiftDate <= endRepeat:
+                    shift = ShiftInstance(baseShift=instance, date=shiftDate, startTime=instance.startTime, endTime=instance.endTime, name=instance.name, description=instance.description, staffSlots=instance.staffSlots, volSlots=instance.volSlots, minSlots=instance.minSlots, slots=instance.slots)
                     shiftList.append(shift)
                     if instance.repeat == 'everyday':
                         shiftDate += timedelta(days=1)
@@ -37,3 +37,31 @@ def create_instance_shifts(sender, instance, created, **kwargs):
                 transaction.commit()
             finally:
                 transaction.set_autocommit(True)
+        
+        else:
+            shift = ShiftInstance.objects.create(baseShift=instance, date=shiftDate, startTime=instance.starTime, endTime=instance.endTime, name=instance.name, description=instance.description, staffSlots=instance.staffSlots, volSlots=instance.volSlots, minSlots=instance.minSlots, slots=instance.slots)
+            shift.save()
+
+@receiver(m2m_changed, sender=BaseShift.defaultStaff.through)
+def default_staff_changed(sender, instance, **kwargs):
+    staffList = [x for x in kwargs['pk_set']]
+    shifts = [x for x in ShiftInstance.objects.filter(baseShift=instance)]
+    shiftStaffRelation = ShiftInstance.staff.through
+
+    relations = []
+    for shift in shifts:
+        relations.extend([shiftStaffRelation(erluser_id=staff, shiftinstance_id=shift.uid) for staff in staffList])
+    
+    shiftStaffRelation.objects.bulk_create(relations, batch_size=100, ignore_conflicts=True)
+
+@receiver(m2m_changed, sender=BaseShift.defaultVols.through)
+def default_vols_changed(sender, instance, **kwargs):
+    volsList = [x for x in kwargs['pk_set']]
+    shifts = [x for x in ShiftInstance.objects.filter(baseShift=instance)]
+    shiftVolsRelation = ShiftInstance.vols.through
+
+    relations = []
+    for shift in shifts:
+        relations.extend([shiftVolsRelation(erluser_id=vol, shiftinstance_id=shift.uid) for vol in volsList])
+
+    shiftVolsRelation.objects.bulk_create(relations, batch_size=100, ignore_conflicts=True)
