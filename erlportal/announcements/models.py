dd92ed73bc5d11709b8c9e185e9f11a7db24a8a0
Constantus <38108928+Constantus@users.noreply.github.com>
2022-10-26 19:11:49 -0400

    Start Announcement model

--- erlportal/announcements/models.py
+++ erlportal/announcements/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Announcement(models.Model):
+    title = models.CharField(max_length=50, blank=False)
+    description = models.TextField(default='', blank=True)
+    dateCreated = models.DateTimeField(default=timezone.now, editable=False)
+    
